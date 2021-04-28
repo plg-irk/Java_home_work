badd2b73e7635ea7393101abd1a3a497e83a29ac
plg-irk <nk-irk@mail.ru>
2021-04-28 22:20:29 +0800

    SecondStep

---
+++ HW1/src/main/java/ru/geekbrains/java/lesson_1/MainApp.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.java.lesson_1;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Здорово, Ленька");
+    }
+
+}
