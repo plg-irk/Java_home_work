bdd1a6d3f96fa2d5d7efed5989fcc8966a8452e2
plg-irk <nk-irk@mail.ru>
2021-05-10 18:52:27 +0800

    HW_of_lesson4_2

--- HomeWorkApp_4/src/main/java/ru/geekbrains/lesson4/HomeWorkApp4.java
+++ HomeWorkApp_4/src/main/java/ru/geekbrains/lesson4/HomeWorkApp4.java
@@ -28,7 +28,7 @@ public class HomeWorkApp4 {
        while (true) {
            human();
            printMap_XO();
-           if (gameWinner("X")) {
+           if (gameWinner("X", valWinner) || gameWinnerD("X", valWinner)) {
                 System.out.println("Вы победили!");
                 break;
            }
@@ -38,7 +38,7 @@ public class HomeWorkApp4 {
            }
            pC();
            printMap_XO();
-           if (gameWinner("O")) {
+           if (gameWinner("O", valWinner)|| gameWinnerD("O", valWinner)) {
                 System.out.println("Победил компьютер");
                 break;
            }
@@ -61,6 +61,7 @@ public class HomeWorkApp4 {
     }
 
     public static boolean emptyFild(int x, int y) {
+// Праверка на возможность хода
         if (x <0 || x > maxHeigth || y<0 || y > maxWidth) {
             System.out.println("Ваш ход за игровым полем");
             return true;
@@ -73,6 +74,7 @@ public class HomeWorkApp4 {
     }
 
     public static void human() {
+// Ход игрока
         int x, y;
         do {
             System.out.println("Введите координату Вашего хода по горизонтали: ");
@@ -84,6 +86,7 @@ public class HomeWorkApp4 {
     }
 
     public static void pC() {
+// Ход компьютера
         int x, y;
         do {
             x = rand.nextInt(maxHeigth);
@@ -93,31 +96,40 @@ public class HomeWorkApp4 {
     }
 
     public static boolean gameOver() {
+// Проверка на наличие пустых ячеек
 
-        int result = 0;
         for (int i = 0; i < maxHeigth; i++) {
             for (int j = 0; j < maxWidth; j++) {
                  if (map_XO[i][j] == emptyCell) {
-                     result ++;
+                     return false;
                  }
             }
         }
-        return result == 0;
+        return true;
 
     }
 
-    public static boolean gameWinner(String str) {
+    public static boolean gameWinner(String str, int a) {
+// Проверка комбинаций по X и Y с учетом того, что
+// комбинация может начинаться не с верхнего ряда,
+// через счетчик, где параметр "a" кол-во комбинаций
+
         boolean result_x = false;
         boolean result_y = false;
-        boolean result_d = false;
         int k;
 
         for (int i = 0; i < maxHeigth; i++) {
             if (result_x) {
                 break;
-            } else {
+            } else {k = 0;
                 for (int j = 0; j < maxWidth; j++) {
-                    if (map_XO[i][j] == str) result_x = true;
+                    if (map_XO[i][j] == str) {
+                        k++;
+                        if (k == a) {
+                            result_x = true;
+                            break;
+                        }
+                    } else if (i == 0 || j == 0 && map_XO[i][j] == emptyCell) continue;
                     else {
                         result_x = false;
                         break;
@@ -129,9 +141,15 @@ public class HomeWorkApp4 {
         for (int i = 0; i < maxWidth; i++) {
             if (result_y) {
                 break;
-            } else {
+            } else {k = 0;
                 for (int j = 0; j < maxHeigth; j++) {
-                    if (map_XO[j][i] == str) result_y = true;
+                    if (map_XO[j][i] == str) {
+                        k++;
+                        if (k == a) {
+                            result_y = true;
+                            break;
+                        }
+                    } else if (i == 0 || j == 0 && map_XO[i][j] == emptyCell) continue;
                     else {
                         result_y = false;
                         break;
@@ -140,34 +158,107 @@ public class HomeWorkApp4 {
             }
         }
 
-        k = 0;
-        for (int i = 0; i < maxWidth; i++) {
-            if (map_XO[i][i] == str) {
-                result_d = true;
-                k ++;
-                if (k == 4) {
-                    break;
-                }
-            }else {
+        return result_x || result_y;
+    }
+
+    public static boolean gameWinnerD(String str, int a) {
+// Проверка комбинаций по диагоналям
+        boolean result_d = false;
+        int k;
+
+        for (int j = 0; j < 2; j++) {
+            if (result_d) break;
+            else {
+                k = 0;
+                for (int i = 0; i < maxWidth - j; i++) {
+                    if (map_XO[i + j][i] == str) {
+                        k++;
+                        if (k == a) {
+                            result_d = true;
+                            break;
+                        }
+                    } else if (i == 0 || j == 0 && map_XO[i+j][j] == emptyCell) continue;
+                    else {
                         result_d = false;
                         break;
                     }
-            }
-
-        k = 0;
-        if (!result_d) {
-            for (int i = 0; i < maxWidth; i++) {
-                if (map_XO[i][maxWidth - i - 1] == str) {
-                    result_d = true;
-                    k ++;
-                    if (k == 4) break;
-                    }else {
-                    result_d = false;
-                    break;
                 }
             }
         }
-        return result_x || result_y || result_d;
+
+        for (int j = 0; j < 2; j++) {
+            if (result_d) break;
+            else {
+                k = 0;
+                for (int i = 0; i < maxWidth - j; i++) {
+                    if (map_XO[i][i+j] == str) {
+                        k++;
+                        if (k == a) {
+                            result_d = true;
+                            break;
+                        }
+                    } else if (i == 0 || j == 0 && map_XO[i][i+j] == emptyCell) continue;
+                    else {
+                        result_d = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int j = 0; j < 2; j++) {
+            if (result_d) break;
+            else {
+                k = 0;
+                for (int i = 0; i < maxWidth - j; i++) {
+                    if (map_XO[i][maxWidth - i - 1 - j] == str) {
+                        k++;
+                        if (k == a) {
+                            result_d = true;
+                            break;
+                        }
+                    } else if (i == 0 || j == 0 && map_XO[i][maxWidth - i - 1 - j] == emptyCell) continue;
+                    else {
+                        result_d = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int j = 0; j < 2; j++) {
+            if (result_d) break;
+            else {
+                k = 0;
+                for (int i = 0; i < maxWidth - j; i++) {
+                    if (map_XO[i+j][maxWidth - i - 1] == str) {
+                        k++;
+                        if (k == a) {
+                            result_d = true;
+                            break;
+                        }
+                    } else if (i == 0 || j == 0 && map_XO[i+j][maxWidth - i - 1] == emptyCell) continue;
+                    else {
+                        result_d = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        return result_d;
     }
+
+    public static void sumCol() {
+        for (int i = 0; i < maxHeigth; i++) {
+            for (int j = 0; j < maxWidth; j++) {
+
+            }
+
+        }
+    }
+
+
 }
 
