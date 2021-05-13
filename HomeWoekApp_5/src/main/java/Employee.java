11b64e86ce02872c24f5735de36c6147758a5ae0
plg-irk <nk-irk@mail.ru>
2021-05-13 23:49:44 +0800

    HW_of_lesson5_1

--- HomeWoekApp_5/src/main/java/Employee.java
+++ HomeWoekApp_5/src/main/java/Employee.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+
+    String name;
+    String position;
+    String email;
+    Integer tel;
+    Integer salary;
+    Integer age;
+
+    Employee(String _name, String _position, String _email, Integer _tel,
+                    Integer _salary, Integer _age) {
+        name = _name;
+        position = _position;
+        email = _email;
+        tel = _tel;
+        salary = _salary;
+        age = _age;
+    }
+
+    void printDataEmployee() {
+
+        System.out.println("Имя сотрудника: " + name + "\n" +
+                "Должность: " + position + "\n"+
+                "Email: " + email + "\n"+
+                "Тел.: " + tel + "\n");
+    }
+
+
+
 }
--- HomeWoekApp_5/src/main/java/HomeWork5.java
+++ HomeWoekApp_5/src/main/java/HomeWork5.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class HomeWork5 {
+public class HomeWork5 {
+
+    public static void main(String[] args) {
+
+        Employee[] arrayEmpl = new Employee[5];
+
+        arrayEmpl[0] = new Employee("Max", "chief", "qwery@tr.ru@",
+                123456780, 150000, 53);
+
+        arrayEmpl[1] = new Employee("Anna", "office-manager", "qwery@tr.ru@",
+                123456781, 50000, 23);
+
+        arrayEmpl[2] = new Employee("Petr", "IT-specialist", "qwery@tr.ru@",
+                123456782, 120000, 23);
+
+        arrayEmpl[3] = new Employee("Oleg", "IT-specialist", "qwery@tr.ru@",
+                123456783, 135000, 41);
+
+        arrayEmpl[4] = new Employee("Maria", "IT-specialist", "qwery@tr.ru@",
+                123456784, 110000, 22);
+
+        for (int i = 0; i < 5; i++) {
+            if (arrayEmpl[i].age > 40 )
+            arrayEmpl[i].printDataEmployee();
+        }
+
+    }
 }
