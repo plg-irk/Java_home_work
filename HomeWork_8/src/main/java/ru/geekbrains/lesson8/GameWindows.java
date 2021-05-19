a55b6701ab26cf5a73180188efd85eeb29cc21cf
plg-irk <nk-irk@mail.ru>
2021-05-19 21:47:30 +0800

    HW_of_lesson8

---
+++ HomeWork_8/src/main/java/ru/geekbrains/lesson8/GameWindows.java
@@ -0,0 +1,2 @@
+package ru.geekbrains.lesson8;public class GameWindows {
+}
---
+++ HomeWork_8/src/main/java/ru/geekbrains/lesson8/MyWindows.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson8;
+
+import javax.swing.*;
+
+
+public class MainWindows extends JFrame {
+
+    private int winHeigth = 420;
+    private int winWigth = 980;
+    
+
+}
